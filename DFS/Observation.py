f00b2af980eecda2d8c848854ee3e41f6fb2d178
tkdalsss <lsm991229@naver.com>
2023-08-16 22:20:35 +0900

    update dfs/bfs 230816(2)

---
+++ DFS/Observation.py
@@ -0,0 +1,67 @@
+from itertools import combinations
+
+n = int(input())
+board = []
+teachers = []
+spaces = []
+
+for i in range(n):
+    board.append(list(input().split()))
+    for j in range(n):
+        if board[i][j] == 'T':
+            teachers.append((i, j))
+        if board[i][j] == 'X':
+            spaces.append((i, j))
+
+def watch(x, y, direction):
+    if direction == 0: # left
+        while y >= 0:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            y -= 1
+    if direction == 1: # right
+        while y < n:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            y += 1
+    if direction == 2: # up
+        while x >= 0:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            x -= 1
+    if direction == 3: # down
+        while x < n:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            x += 1
+    return False
+
+def process():
+    for x, y in teachers:
+        for i in range(4):
+            if watch(x, y, i):
+                return True
+    return False # 선생님이 학생을 감지 못하는 경우
+
+find = False # 선생님이 학생을 감지 못하는 경우를 찾았을 때 기본은 False
+for data in combinations(spaces, 3):
+    for x, y in data:
+        board[x][y] = 'O'
+    if not process():
+        find = True
+        break
+    for x, y in data:
+        board[x][y] = 'X'
+
+if find:
+    print('YES')
+else:
+    print('NO')
