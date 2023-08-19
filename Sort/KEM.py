239d1b73cf7e1a613b9b0f455b9785aa75680b5f
tkdalsss <lsm991229@naver.com>
2023-08-19 21:18:12 +0900

    upload sort 230818

---
+++ Sort/KEM.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+data = []
+for _ in range(n):
+    data.append(input().split())
+
+data.sort(key = lambda x : (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for i in data:
+    print(i[0])
