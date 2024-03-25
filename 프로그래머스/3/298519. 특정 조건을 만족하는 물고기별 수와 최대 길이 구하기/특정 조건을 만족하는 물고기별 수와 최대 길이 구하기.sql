8d701ef3228f6c5db521bdfe87abbd0ed0e7b2c7
tkdalsss <lsm991229@naver.com>
2024-03-25 23:47:49 +0900

    Update 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
    10cm이하의 물고기 조건 추가

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,6 +1,8 @@
 -- 코드를 작성해주세요
-select count(*) as fish_count, max(length) as max_length, fish_type
+select count(*) as fish_count, 
+max(case when length <= 10 then 10 else length end) as max_length, 
+fish_type
 from fish_info
 group by fish_type
-having avg(length) >= 33
+having avg(case when length <= 10 then 10 else length end) >= 33
 order by fish_type;
