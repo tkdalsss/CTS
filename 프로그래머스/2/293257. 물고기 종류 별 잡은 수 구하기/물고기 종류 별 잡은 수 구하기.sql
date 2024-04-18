91bf7dcd545083e4a0fc03dd8a0b2f527576a914
이상민 <lsm991229@naver.com>
2024-04-18 11:18:55 +0900

    [level 2] Title: 물고기 종류 별 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 작성해주세요
+select count(*) as fish_count, fni.fish_name
+from fish_info fi join fish_name_info fni on fi.fish_type = fni.fish_type
+group by fni.fish_name
+order by fish_count desc;
