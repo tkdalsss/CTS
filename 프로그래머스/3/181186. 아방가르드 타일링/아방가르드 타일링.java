aac788b2d0958f23a555ce5bca44d19f3e1654f9
이상민 <lsm991229@naver.com>
2024-05-01 13:45:37 +0900

    [level 3] Title: 아방가르드 타일링, Time: 27.35 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.java
@@ -0,0 +1,34 @@
+class Solution {
+    public final int MOD = 1000000007;
+    public int solution(int n) {
+        // int answer = 0;
+        long[] dp = new long[n+1];
+        long[] dp4 = new long[n+1];
+        long[] dp5 = new long[n+1];
+        long[] dp6 = new long[n+1];
+        dp[0] = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            if (i-1 >= 0) dp[i] = (dp[i] + dp[i-1]) % MOD;
+            if (i-2 >= 0) dp[i] = (dp[i] + dp[i-2] * 2) % MOD;
+            if (i-3 >= 0) dp[i] = (dp[i] + dp[i-3] * 5) % MOD;
+            if (i-4 >= 0) {
+                long diff = (dp[i-4] * 2) % MOD;
+                dp4[i] = (dp4[i-3] + diff) % MOD;
+                dp[i] = (dp[i] + dp4[i]) % MOD;
+            }
+            if (i-5 >= 0) {
+                long diff = (dp[i-5] * 2) % MOD;
+                dp5[i] = (dp5[i-3] + diff) % MOD;
+                dp[i] = (dp[i] + dp5[i]) % MOD;
+            }
+            if (i-6 >= 0) {
+                long diff = (dp[i-6] * 4) % MOD;
+                dp6[i] = (dp6[i-3] + diff) % MOD;
+                dp[i] = (dp[i] + dp6[i]) % MOD;
+            }
+        }
+        
+        return Math.toIntExact(dp[n]);
+    }
+}
